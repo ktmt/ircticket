f85936225b9c18ed2997bf16b724dd91318f2cb6
dxhuy1988 <doxuanhuy@gmail.com>
2013-05-29 20:26:12 +0900

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ircticket::Application.config.secret_token = '1a21bf044d69db34c7f7b985e6afc7eab693bbb61c5626b8f27264655e465841ad6aab7ecd6b3e62d3e38ae757b3cf42e8b2d59b6064123971ae5bc022e453ab'
